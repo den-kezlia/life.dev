4ac1e714fc0b8482396d2b8e3c8d298407076150
home-work <xian.den@gmail.com>
2015-03-24 16:12:10 +0200

    Show layout 10x10

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
     App = {
+        rows: 10,
+        columns: 10,
+
         init: function() {
-            console.log('life');
+            this.initLayout();
+        },
+
+        initLayout: function() {
+            var layoutHtml = $('<div class="life-wrapper" id="lifeMatrix" />');
+
+            for (var row = 1; row <= App.rows; row++) {
+                var line = $('<ul class="life-row" />');
+                for (var column = 1; column <= App.columns; column++) {
+                    var cell = $('<li class="life-cell">' + row + '-' + column + '</li>');
+                    line.append(cell);
+                }
+
+                layoutHtml.append(line);
+            }
+
+            $('body').append( layoutHtml );
         }
     };
 
